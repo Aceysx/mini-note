e8d3601b0f656bdbaee489ec627c63fcf971feaa
Aceysx <aceysx@163.com>
2021-12-05 15:35:29 +0800

    feat: file action menu

--- src/layouts/sidebar.tsx
+++ src/layouts/sidebar.tsx
@@ -4,17 +4,13 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Collapse from "@mui/material/Collapse";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import DraftsIcon from "@mui/icons-material/Drafts";
-import SendIcon from "@mui/icons-material/Send";
-import ExpandLess from "@mui/icons-material/ExpandLess";
-import ExpandMore from "@mui/icons-material/ExpandMore";
 import { connect } from "umi";
 import GlobalModel, { GlobalModelState, SiderbarState } from "@/models/global";
 import FileModel from "@/models/file";
 import FolderIcon from "@mui/icons-material/Folder";
 import FolderOpenIcon from "@mui/icons-material/FolderOpen";
-import { ListSubheader } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import { Menu } from "@mui/material";
 
 const Sidebar = ({
   siderbarState
@@ -23,6 +19,33 @@ const Sidebar = ({
   siderbarState: SiderbarState;
 }) => {
   const { rootFile, dirsOpenState } = siderbarState;
+  const [contextMenu, setContextMenu] = React.useState<{
+    position: {
+      mouseX: number;
+      mouseY: number;
+    };
+    file: FileModel;
+  } | null>(null);
+
+  const handleContextMenu = (event: React.MouseEvent, file: FileModel) => {
+    event.preventDefault();
+    setContextMenu(
+      contextMenu === null
+        ? {
+            position: {
+              mouseY: event.clientY - 4,
+              mouseX: event.clientX - 2
+            },
+            file
+          }
+        : null
+    );
+    return false;
+  };
+
+  const handleClose = () => {
+    setContextMenu(null);
+  };
 
   const isOpen = (path: string): boolean => {
     // @ts-ignore
@@ -47,7 +70,13 @@ const Sidebar = ({
 
             const node = (
               <div>
-                <ListItemButton sx={{ pl: depth }} key={file.path}>
+                <ListItemButton
+                  sx={{ pl: depth }}
+                  key={file.path}
+                  onClick={() => clickDirItem(file)}
+                  onContextMenu={e => handleContextMenu(e, file)}
+                  style={{ cursor: "context-menu" }}
+                >
                   <ListItemIcon>
                     {dirsOpenState[file.path] ? (
                       <FolderOpenIcon />
@@ -56,7 +85,6 @@ const Sidebar = ({
                     )}
                   </ListItemIcon>
                   <ListItemText
-                    onClick={() => clickDirItem(file)}
                     primary={
                       <span style={{ fontSize: 14 }}>
                         {file.parseDisplayName()}
@@ -64,6 +92,28 @@ const Sidebar = ({
                     }
                   />
                 </ListItemButton>
+                <Menu
+                  className={"bg_BG"}
+                  open={contextMenu !== null}
+                  onClose={handleClose}
+                  anchorReference="anchorPosition"
+                  aria-haspopup="true"
+                  anchorPosition={
+                    contextMenu !== null
+                      ? {
+                          top: contextMenu.position.mouseY,
+                          left: contextMenu.position.mouseX
+                        }
+                      : undefined
+                  }
+                >
+                  <MenuItem onClick={handleClose}>Edit</MenuItem>
+                  <MenuItem onClick={handleClose}>Delete</MenuItem>
+
+                  <MenuItem onClick={handleClose}>New File</MenuItem>
+                  <MenuItem onClick={handleClose}>New Directory</MenuItem>
+                </Menu>
+
                 {isOpen(file.path)
                   ? subFiles.map(item => (
                       <ListItemButton sx={{ pl: depth + 2 }} key={item.path}>
